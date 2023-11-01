b0463dbbd1f5af52c6736b3db9d891b12cf1b533
kirill pavlov <2020kirillp@gmail.com>
2023-10-31 18:15:48 -0700

    Initial Commit

---
+++ countdown/countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownApp.swift
+//  countdown
+//
+//  Created by Kirill Pavlov on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
